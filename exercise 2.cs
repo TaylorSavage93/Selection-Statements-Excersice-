19b23a143fa1696c4460d0b7441da0a4f899df47
Taylor Savage <taylorsavage93@gmail.com>
2022-07-07 13:34:22 -0400

    exercise 2 correct file

---
+++ exercise 2.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("What is your favorite school subject?");
+var subject = Console.ReadLine();
+
+switch (subject)
+{
+    case "Math":
+        Console.WriteLine("Math is kind of hard!");
+        break;
+    case "Science":
+        Console.WriteLine("Science is my favorite also!");
+        break;
+    case "English":
+        Console.WriteLine("English is a good subject to know!");
+        break;
+    case "History":
+        Console.WriteLine("History is a tough subject!");
+        break;
+    case "PE":
+        Console.WriteLine("PE is a fun class!");
+        break;
+    default:
+        Console.WriteLine("Thats a cool subject");
+        break;
+
+
+
+}
