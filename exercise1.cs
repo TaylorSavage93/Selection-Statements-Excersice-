216c38d40e0f0b7bcafe4f63ea0142b47f93b3ce
Taylor Savage <taylorsavage93@gmail.com>
2022-07-07 14:10:08 -0400

    had wrong files uploaded deleted and fixed

---
+++ exercise1.cs
@@ -0,0 +1,23 @@
+﻿#region
+var r = new Random();
+var favNumber = r.Next(1, 1000);
+
+Console.WriteLine("Please enter your guess");
+var userInput = int.Parse(Console.ReadLine());
+
+if (userInput < favNumber) 
+{
+Console.WriteLine($"{userInput} is too low");
+}
+else if (userInput > favNumber) 
+{
+    Console.WriteLine($"{userInput} is too high");
+}
+else if (userInput == favNumber)
+{
+    Console.WriteLine($"{userInput} is correct! congratulations!");
+}
+
+
+
+#endregion
